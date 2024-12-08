65d58cc23b62ed882f0ca8f245703b2a622c9b10
Kliver59 <christopher59880@hotmail.fr>
2024-12-08 15:58:35 +0100

    changements

--- site_perso3/server.js
+++ site_perso3/server.js
@@ -11,21 +11,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Utilisation de __dirname pour accéder au répertoire courant
+// Utilisation de __dirname pour accéder au répertoire courant et servir des fichiers statiques
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route API pour les tâches
 app.use("/api/v1/tasks", taskRouter);
 
-// Route pour le fichier contact.html
+// Route pour la page d'accueil
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "projet_perso.html"));
 });
 
-// Démarrer le serveur
-const PORT = process.env.PORT || 4000;
+// Démarrer le serveur sur le port dynamique attendu par Vercel
+const PORT = process.env.PORT || 3000; // Vercel attend généralement le port 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Exporter le PORT si nécessaire
+// Exporter le PORT si nécessaire (utile dans certains cas)
 module.exports = PORT;
