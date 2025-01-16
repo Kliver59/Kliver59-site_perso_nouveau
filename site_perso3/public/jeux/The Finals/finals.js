e3ecc3aa5bff7d8e75ad7306e2d736d059b0fc76
Kliver59 <christopher59880@hotmail.fr>
2025-01-16 13:48:11 +0100

    supressions de the finals

--- site_perso3/public/jeux/The Finals/finals.js
+++
@@ -1,7 +0,0 @@
-function playMusic() {
-  var audio = document.getElementById("background-audio");
-  audio.play();
-}
-
-// Démarre la musique lorsque la page est chargée
-window.onload = playMusic;
