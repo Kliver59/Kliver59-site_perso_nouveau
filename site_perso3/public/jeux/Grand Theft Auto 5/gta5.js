955c1fb10fb29f676cd69c6e5f66d4035f1ce924
Kliver59 <christopher59880@hotmail.fr>
2025-01-27 15:47:25 +0100

    modification page gta 5

--- site_perso3/public/jeux/Grand Theft Auto 5/gta5.js
+++ site_perso3/public/jeux/Grand Theft Auto 5/gta5.js
@@ -3,10 +3,33 @@ function playMusic() {
   audio.play();
 }
 
-// Démarre la musique lorsque la page est chargée
+function pauseMusic() {
+  var audio = document.getElementById("background-audio");
+  audio.pause();
+}
+
 window.onload = playMusic;
 
 document.querySelector(".menu-toggle").addEventListener("click", () => {
   const navBar = document.querySelector(".Nav-Bar");
   navBar.classList.toggle("open");
+});
+
+document.getElementById("play-music").addEventListener("click", () => {
+  document.getElementById("background-audio").play();
+});
+
+document.getElementById("pause-music").addEventListener("click", () => {
+  document.getElementById("background-audio").pause();
+});
+
+const audio = document.getElementById("background-audio");
+const progress = document.getElementById("music-progress");
+
+audio.addEventListener("timeupdate", () => {
+  progress.value = (audio.currentTime / audio.duration) * 100;
+});
+
+progress.addEventListener("input", () => {
+  audio.currentTime = (progress.value / 100) * audio.duration;
 });
